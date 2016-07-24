bbddb0e977339562fab1a44ea8d2f49d6857541f
Marko Sanković <sankovic.marko@gmail.com>
2016-07-24 23:07:59 +0200

    Initial commit

---
+++ app/src/main/java/com/synapticon/synalampproto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.synapticon.synalampproto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
